bc9c521b3475c557699fe46c041bd4f028f04ef0
VictorVivasIntevo <victor.vivas@intevo.com.co>
2026-02-17 16:11:49 -0500

    Refactor code structure for improved readability and maintainability

---
+++ frontend/src/app/features/dashboard/presentation/pages/comparative-social-network.page/comparative-social-network.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Card } from "@/shared/ui/components/card/card";
+
+@Component({
+  selector: 'app-comparative-social-network.page',
+  imports: [Card],
+  templateUrl: './comparative-social-network.page.html',
+  styles: ``,
+})
+export class ComparativeSocialNetworkPage {
+
+}
