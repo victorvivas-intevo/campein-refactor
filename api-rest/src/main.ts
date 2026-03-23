5b6a62a408f7ae6178206f605c8311914277daff
csilva39 <csilva39@estudiantes.areandina.edu.co>
2026-03-23 12:54:01 -0500

    test ci

--- api-rest/src/main.ts
+++ api-rest/src/main.ts
@@ -8,10 +8,10 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     // origin: ['https://campein-frontend-408168404729.us-central1.run.app'], // o un array de orígenes si necesitas más
-    origin: ['http://localhost:4200'], // o un array de orígenes si necesitas más
+    origin: ['https://prod-front-campein-408168404729.us-east1.run.app'], // o un array de orígenes si necesitas más
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
 }
 bootstrap();
